8618e671a347504fe721511f38611b05b2c2c6f1
Dimit Chadha <java.pathshala@gmail.com>
2017-08-08 13:31:43 +0530

    Hello World Application

---
+++ Spring/Boot/src/main/java/com/jp/spring/boot/hello/HelloController.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Java Pathshala.
+ * All rights reserved.
+ *
+ * This program is protected by copyright law but you are authorise to learn
+ * & gain ideas from it. Its unauthorised use is explicitly prohibited &
+ * any addition & removal of material. If want to
+ * suggest any changes, you are welcome to provide your comments on GitHub
+ * Social Code Area. Its unauthorised use gives Java Pathshala the right to
+ * obtain retention orders and to prosecute the authors of any infraction.
+ * www.javapathshala.com
+ */
+package com.jp.spring.boot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ *
+ *
+ * @author Dimit Chadha
+ * @since 1.0
+ * @version 1.0
+ */
+@Controller
+@EnableAutoConfiguration
+public class HelloController
+{
+
+    @RequestMapping("/")
+    @ResponseBody
+    //http://localhost:8080/
+    String home()
+    {
+        return "Welcome to Spring Boot!";
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(HelloController.class, args);
+    }
+}
