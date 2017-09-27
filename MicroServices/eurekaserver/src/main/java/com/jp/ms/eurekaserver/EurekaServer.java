8b89b957508d3030a635c061076b94cda7bfd959
Dimit Chadha <java.pathshala@gmail.com>
2017-09-27 14:57:35 +0530

    eureka fault

---
+++ MicroServices/eurekaserver/src/main/java/com/jp/ms/eurekaserver/EurekaServer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jp.ms.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author DChadha
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+
+    }
+}
